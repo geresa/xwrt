jacelift/x-wrt_jacelift

ath79: init support for xwrt_m-ap300g

target/linux/ath79/image/generic.mk
@@ -711,6 +711,14 @@ define Device/xwrt_mk-v0201
 endef
 TARGET_DEVICES += xwrt_mk-v0201
 
+define Device/xwrt_m-ap300g
+  ATH_SOC := ar9344
+  DEVICE_TITLE := XWRT M-AP300G
+  DEVICE_PACKAGES := kmod-leds-gpio kmod-usb-serial-pl2303 kmod-usb-core kmod-usb2 kmod-usb-ledtrig-usbport
+  IMAGE_SIZE := 16000k
+endef
+TARGET_DEVICES += xwrt_m-ap300g
+
 define Device/yuncore_a770
   ATH_SOC := qca9531
   DEVICE_TITLE := YunCore A770

geresa/xwrt

target/linux/ath79/image/generic.mk
@@ -2404,6 +2404,15 @@ define Device/xwrt_csac2
 endef
 TARGET_DEVICES += xwrt_csac2
 
+define Device/xwrt_m-ap300g
+  SOC := ar9344
+  DEVICE_VENDOR := XWRT
+  DEVICE_MODEL := M-AP300G
+  DEVICE_PACKAGES := kmod-leds-gpio kmod-usb-serial-pl2303 kmod-usb-core kmod-usb2 kmod-usb-ledtrig-usbport base-settings-xwrt-m-ap300g
+  IMAGE_SIZE := 16000k
+endef
+TARGET_DEVICES += xwrt_m-ap300g
+
 define Device/yuncore_a770
   SOC := qca9531
   DEVICE_VENDOR := YunCore